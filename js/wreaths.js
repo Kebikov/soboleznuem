c423be9f284896d959d2026a779affd5d8c5e222
Gen <kebikovgen@gmail.com>
2022-07-10 16:45:00 +0300

    changed text box-text__span

---
+++ js/wreaths.js
@@ -0,0 +1,142 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const 
+    shopBlock = document.querySelector('.shop-block'),
+    buttonSort = document.querySelector('.button-sort'),
+    inputPrace = document.querySelector('.button-sort__praice-box'),
+    inputFrom = document.querySelector('[name="from"]'),
+    inputBefore = document.querySelector('[name="before"]');
+
+    let 
+    inputBeforeValue = 0, 
+    inputFromValue = 0;
+    
+    //= function 
+    //вывод на экран
+    function inputCoffins (arr) {
+        arr.forEach(item => {
+            shopBlock.insertAdjacentHTML('beforeend',`
+            <div class="shop-block">    
+                <div class="shop-block__item  flex-wreaths">
+                    <div class="card">
+                        <div class="card__body">
+                            <div class="card__title">${item.title}</div>
+                            <div class="card__img card__img_height-wreaths">
+                                <picture>
+                                    <source srcset=${item.img1} type="image/webp">
+                                    <img src=${item.img2} alt="Венок ритуальный">
+                                </picture>
+                        </div>
+                        <div class="card__prais">${item.prais}</div>
+                        <a class="card__button" href="contacts.html">купить</a>
+                    </div>
+                </div>
+            </div>
+            `);
+        });
+    }
+
+    function sortArreyMinMax (arr) {
+        return arr.sort(fnSort);
+    }
+
+    function sortArreyMaxMin (arr) {
+        return sortArreyMinMax(arr).reverse();
+    }
+
+    function fnSort (x, y) {
+        let a = +`${x.prais}`.match(/\d+.?\d+?/g).join('');
+        let b = +`${y.prais}`.match(/\d+/g).join('');
+        if(a > b) return 1;
+        if(a < b) return -1;
+        if(a === b) return 0;
+    }
+
+    //очистка всего магазина
+    function delShopElements () {
+        shopBlock.innerHTML = '';
+    }
+
+    function removeClassButton (thisTarget) {
+        document.querySelectorAll('._sort-color').forEach(item => {
+            item.classList.remove('_sort-color');
+        });
+        thisTarget.classList.add('_sort-color');
+    }
+
+    function sortNum (fn) {
+        if(!inputFrom.value) {
+            inputFromValue = 0;
+        }else {
+            inputFromValue = +inputFrom.value;
+        } 
+        if(!inputBefore.value) {
+            inputBeforeValue = 10000;
+        }else {
+            inputBeforeValue = +inputBefore.value;
+        }
+        fetch('ajax/wreaths.json')
+        .then(data => data.json())
+        .then(objArrey => {
+            shopBlock.innerHTML = '';
+            let inputSort = objArrey.filter((item, key, arr) => {
+                let numSort = +(item.prais).match(/\d+.?\d+?/g).join('');
+                if (inputBeforeValue > numSort && numSort > inputFromValue) {
+                    return arr;
+                }
+            });
+            if(!inputSort.length) {
+                shopBlock.insertAdjacentHTML('beforeend', '<div class="shop-block__not">Извините, нет подходящих товаров.</div>');
+            } 
+            inputCoffins(fn(inputSort));
+            });
+    }
+
+    //= CODE 
+
+    fetch('ajax/wreaths.json')
+    .then(data => data.json())
+    .then(objArrey => {
+        inputCoffins(sortArreyMaxMin(objArrey));
+    });
+
+    buttonSort.addEventListener('click', (e) => {
+        let target = e.target;
+        if(target.classList.contains('button-sort__cheap')) {
+            removeClassButton(target);
+            sortNum(sortArreyMinMax);
+        }
+        if(target.classList.contains('button-sort__lot')) {
+            removeClassButton(target);
+            sortNum(sortArreyMaxMin);
+        }
+    });
+
+    inputPrace.addEventListener('input', () => {
+
+        let maxOrMin = document.querySelector('._sort-color');
+
+        if(maxOrMin.classList.contains('button-sort__lot')) {
+            sortNum(sortArreyMaxMin);
+        }else {
+            sortNum(sortArreyMinMax);
+        }
+    });
+
+    // //получение карточек
+    // const shopBlockItems = document.querySelectorAll('.shop-block__item');
+    // let cartArr = [];
+    // shopBlockItems.forEach(item => {
+    //     let objectCart = {};
+    //     objectCart.title = item.querySelector('.card__title').innerHTML;
+    //     objectCart.img1 = item.querySelector('source').getAttribute('srcset');
+    //     objectCart.img2 = item.querySelector('img').getAttribute('src');
+    //     objectCart.prais = item.querySelector('.card__prais').innerHTML;
+    //     cartArr.push(objectCart);
+    // });
+    // const jsonCardArr = JSON.stringify(cartArr);
+    // //console.log('',jsonCardArr);
+
+});
